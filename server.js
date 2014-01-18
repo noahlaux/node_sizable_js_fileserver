61df45cf620b93b7ef80901cac706f4f809b4b61
Noah Laux <noahlaux@gmail.com>
2014-01-18 14:43:38 -0500

    Added lodash and package file

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 /**
  * Creates javascript files of various size
- * 
+ *
  * @example http://[YOUR URL]/?size=20000
- * 
+ *
  * @author Noah Laux (noahlaux@gmail.com)
  *
- */ 
-var http = require('http'),
-    url = require("url"),
-    util = require('util'),
-    _    = require("underscore"),
-    mu   = require('mu2');
+ * @require mu2, underscore
+ *
+ */
+var http    = require('http'),
+    url     = require("url"),
+    util    = require('util'),
+    _       = require("lodash"),
+    mu      = require('mu2');
 
 mu.root = __dirname + '/templates';
 
 http.createServer( function ( req, res ) {
-    
+
     var query   = url.parse( req.url, true ).query,
-        size    = query.size ? query.size : 200,
-        string  = _.map( _.range( 0 , size ), function(){ 
-            return "lorem ipsum ";
-        }).join("");
-    
+        size    = query.size || 200,
+        string  = _.map( _.range( 0 , size ), function(){
+            return 'lorem ipsum ';
+        }).join('');
+
     mu.clearCache();
-    
-    var stream = mu.compileAndRender('standard.mustache', {
+
+    var stream = mu.compileAndRender( 'standard.mustache', {
         string: string
     });
-        
+
     res.writeHead( 200, {
         'Content-Type': 'application/javascript'
     });
-      
+
     util.pump(stream, res);
 
 }).listen( process.env.PORT );
