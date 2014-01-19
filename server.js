0f7b9f76727b2b05b128386dd1245d961377c20d
Noah Laux <noahlaux@gmail.com>
2014-01-18 19:32:59 -0500

    Added support for CSS and modular expansion

--- server.js
+++ server.js
@@ -5,13 +5,12 @@
  *
  * @author Noah Laux (noahlaux@gmail.com)
  *
- * @require mu2, underscore
+ * @require mu2
  *
  */
 var http    = require('http'),
     url     = require("url"),
     util    = require('util'),
-    _       = require("lodash"),
     mu      = require('mu2');
 
 mu.root = __dirname + '/templates';
@@ -19,19 +18,32 @@ mu.root = __dirname + '/templates';
 http.createServer( function ( req, res ) {
 
     var query   = url.parse( req.url, true ).query,
-        size    = query.size || 200,
-        string  = _.map( _.range( 0 , size ), function(){
-            return 'lorem ipsum ';
-        }).join('');
+        types   = {
+            'js': {
+                type: 'js',
+                mimeType: 'application/javascript'
+            },
+            'css': {
+                type: 'css',
+                mimeType: 'text/css'
+            }
+        },
+        size    = parseInt( query.size, 10 ) || 200,
+        type    = types[ query.type ] ? types[ query.type ].type : 'js',
+        data    = new Array( size ).join('.').split('.').map(function ( key, i ) {
+            return {
+                raw: i
+            };
+        });
 
     mu.clearCache();
 
-    var stream = mu.compileAndRender( 'standard.mustache', {
-        string: string
+    var stream = mu.compileAndRender( type + '.mustache', {
+        "data": data
     });
 
     res.writeHead( 200, {
-        'Content-Type': 'application/javascript'
+        'Content-Type': type.mimeType
     });
 
     util.pump(stream, res);
